da4129a8c0d23effb56dbade92d670685917fcb7
Chris <0144266@studentmail.ul.ie>
2014-03-07 14:31:36 +0000

    Added mail.properties

--- monaleen-tennis/src/main/java/controllers/HomeController.java
+++ monaleen-tennis/src/main/java/controllers/HomeController.java
@@ -1,10 +1,10 @@
 package controllers;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 
 @Controller("homeController")
@@ -12,11 +12,9 @@ public class HomeController {
 	
 	private static Logger logger = Logger.getLogger(HomeController.class); 
 	
-	
 	@RequestMapping("/")
 	public String showHome() {
 		final String currentUser = SecurityContextHolder.getContext().getAuthentication().getName();
-	
 		logger.info("Showing Home Page...." + currentUser);
 		return "index";
 	}
